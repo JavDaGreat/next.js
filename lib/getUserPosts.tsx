a99c80f61271250a8b727e7a6d4792b1ab75e9ee
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-10-07 11:55:04 +0200

    update

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,7 +1,7 @@
 export default async function GetUsersPost(userId: string) {
   const resp = await fetch(
     `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
-    { next: { revalidate: 60 } }
+    { next: { revalidate: 120 } }
   );
   if (!resp.ok) return undefined;
 
